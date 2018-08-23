338923b5dfde7584c77847479c3ea9e2636a789c
Rails Udemy Course <potter.michael@gmail.com>
2018-08-23 11:51:18 +0000

    addded destroy action, crud complete?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to article_path (@article)
+      redirect_to article_path(@article)
     else 
       render 'new'
     end
@@ -36,6 +36,13 @@ class ArticlesController < ApplicationController
   
   def show 
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article was successfully deleted"
   end
   
   private 
