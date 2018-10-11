ecc3110a32e80bf20e5e55bb1895bcb6cc87d89c
Rails Udemy Course <potter.michael@gmail.com>
2018-10-11 00:48:03 +0000

    added update functionality and partial for update and new form views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
       @user = User.new
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Your account has been updated successfully"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
+    end
   
     def create
       @user = User.new(user_params)
