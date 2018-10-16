8fdd85aa95ba297a917734d1d9ce71814ba63765
Rails Udemy Course <potter.michael@gmail.com>
2018-10-16 23:14:43 +0000

    create show action and add gravitar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
       end
     end
   
+    def show
+      @user = User.find(params[:id])
+    end
+  
+  
     def create
       @user = User.new(user_params)
       if @user.save
